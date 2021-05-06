866c6dc94411ea4d69a3f4b1daf44ab0074201bb
whimzyLive <rp970.patel@gmail.com>
2021-05-06 12:27:19 +0930

    add support for optional case sensitive param

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,22 @@
+use std::env;
 use std::error::Error;
 use std::fs;
 
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_name)?;
-    println!("With text:\n{}", contents);
 
+    let results = search(&config.query, &contents, config.case_sensitive);
+
+    for result in results {
+        println!("{}", result);
+    }
     Ok(())
 }
 
 pub struct Config {
     pub query: String,
     pub file_name: String,
+    pub case_sensitive: bool,
 }
 
 impl Config {
@@ -22,6 +28,63 @@ impl Config {
         let query = args[1].clone();
         let file_name = args[2].clone();
 
-        Ok(Config { query, file_name })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
+        Ok(Config {
+            query,
+            file_name,
+            case_sensitive,
+        })
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str, case_sensitive: bool) -> Vec<&'a str> {
+    let mut lines_found = vec![];
+
+    for line in contents.lines() {
+        let mut query_to_search = query;
+        let query_lowercase = &query.to_lowercase();
+
+        // compare lower case string
+        if !case_sensitive {
+            query_to_search = query_lowercase;
+        }
+
+        if line.contains(query_to_search) {
+            lines_found.push(line.trim());
+        }
+    }
+    lines_found
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_query() {
+        let query = "duct";
+        let contents = "\
+        Rust,
+        Safe, fast and productive
+        Pick all three.
+        ";
+
+        assert_eq!(
+            vec!["Safe, fast and productive"],
+            search(query, contents, false)
+        );
+    }
+
+    #[test]
+    fn search_query_insensitive() {
+        let query = "Productive";
+        let contents = "\
+        Rust,
+        Safe, fast and productive
+        Pick all three.
+        ";
+
+        assert_eq!(vec![] as Vec<String>, search(query, contents, true));
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Failed to parse input: {}", err);
+        eprintln!("Failed to parse input: {}", err);
         process::exit(1);
     });
 
     if let Err(err) = run(config) {
-        println!("Application err: {}", err);
+        eprintln!("Application err: {}", err);
     }
 }
